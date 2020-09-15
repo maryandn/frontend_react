d1d5f991efeaf41688dd381d63533968a29b64b1
bigbird <maryandnmail@gmail.com>
2020-09-15 17:38:39 +0300

    15.09.2020 - practic b

--- src/navbar/inputform.js
+++ src/navbar/inputform.js
@@ -13,139 +13,134 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function BasicTextFields(props) {
-    const [login, setLogin] = useState({
-        login: "",
-        password: ""
-    })
 
-    const [signup, setSignup] = useState({
+    const [form, setForm] = useState({
         login: "",
         password: "",
         confirmPassword: "",
         email: "",
         phone: "",
-        emailValidate: false
+        emailValidate: false,
+        input: props.type
+
     })
 
-    const handleChangeLogin = e => {
+    const handleChangeForm = e => {
         const {id, value} = e.target
-        setLogin(prevState => ({
+        setForm(prevState => ({
             ...prevState,
             [id]: value
         }))
     }
 
-    const handleChangeSignup = e => {
-        setSignup(prevSignup => {
-            return {
-                ...prevSignup,
-                login: e.target.login,
-            }
-        })
-    }
+    const handleSubmitForm = () => {
 
-    const handleSubmitLogin = () => {
+        const body = (
+        if (form.input === 'Войти') {
+            return JSON.stringify({})
+        } else {
+            return JSON.stringify({})
+        }
+        )
+
+
+
+        const bodyLogin = {
+            "username": form.login,
+            "password": form.password,
+            "input": form.input
+        }
+
+        const bodySignup = {
+            "username": form.login,
+            "password": form.password,
+            "mail": form.email,
+            "input": form.input,
+            "profile": {
+                "phone": form.phone
+            }
+        }
 
         const cors = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                "username": login.login,
-                "password": login.password
-            })
-        };
-        fetch("http://127.0.0.1:8000/token/", cors)
-            .then(response => response.json())
-            .then(data => {
-                if (data.access) {
-                    localStorage.setItem("token", data.access)
-                } else {
-                    console.log(data)
+            body: JSON.stringify(
+                function () {
+                    if (form.input === 'Войти') {
+                        console.log(bodyLogin)
+                        return {bodyLogin}
+                    } else {
+                        console.log(bodySignup)
+                        return {bodySignup}
+                    }
                 }
-            })
-            .catch(function (error) {
-                console.log(error)
-            });
+            )
+        };
+        console.log(cors.body)
+        // fetch("http://127.0.0.1:8000/token/", cors)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         if (data.access) {
+        //             localStorage.setItem("token", data.access)
+        //         } else {
+        //             console.log(data)
+        //         }
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error)
+        //     });
         props.updateHandleClose(false)
     }
 
-    const handleSubmitSignup = () => {
-        console.log(signup)
-        props.updateHandleClose(false)
-    };
-
     const classes = useStyles();
-    const type = props.type
-    if (type === 'Войти') {
-        return (
-            <form className={classes.root} noValidate autoComplete="off">
-                <p>{login.login ? '' : 'not'}</p>
-                <TextField id="login"
-                           label='Login'
-                           variant="outlined"
-                           required={true}
-                           value={login.login}
-                           onChange={handleChangeLogin}
-                />
-                <p>{login.password ? '' : 'not'}</p>
-                <TextField id="password"
-                           label='Password'
-                           variant="outlined"
-                           required={true}
-                           value={login.password}
-                           onChange={handleChangeLogin}
-                />
-                <Button variant="contained" color="primary" onClick={handleSubmitLogin}>
-                    Войти
-                </Button>
-            </form>
-        );
-    } else {
-        return (
-            <form className={classes.root} noValidate autoComplete="off">
-                <TextField id="signupLogin"
-                           label='Login'
-                           variant="outlined"
-                           required={true}
-                           value={signup.login}
-                           onChange={handleChangeSignup}
-                />
-                <TextField id="signupPassword"
-                           label='Password'
-                           variant="outlined"
-                           required={true}
-                           value={signup.password}
-                           onChange={handleChangeSignup}
-                />
-                <p>{ signup.password === signup.confirmPassword ? '' : 'пароли не совпадают'}</p>
+
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="login"
+                       label='Login'
+                       variant="outlined"
+                       required={true}
+                       value={form.login}
+                       onChange={handleChangeForm}
+            />
+            <TextField id="password"
+                       label='Password'
+                       variant="outlined"
+                       required={true}
+                       value={form.password}
+                       onChange={handleChangeForm}
+            />
+            {form.input === 'Войти' ? '' :
                 <TextField id="confirmPassword"
                            label='Confirm Password'
                            variant="outlined"
                            required={true}
-                           value={signup.confirmPassword}
-                           onChange={handleChangeSignup}
-                />
+                           value={form.confirmPassword}
+                           onChange={handleChangeForm}
+                /> }
+            {form.input === 'Войти' ? '' :
                 <TextField id="email"
                            label='Email'
                            variant="outlined"
                            required={true}
-                           value={signup.email}
-                           onChange={handleChangeSignup}
-                />
+                           value={form.email}
+                           onChange={handleChangeForm}
+                />}
+            {form.input === 'Войти' ? '' :
                 <TextField id="phone"
                            label='Phone'
                            variant="outlined"
                            required={true}
-                           value={signup.phone}
-                           onChange={handleChangeSignup}
-                />
-                <Button variant="contained" color="primary" onClick={handleSubmitSignup}>
-                    Зарегестрироватся
-                </Button>
-            </form>
-        );
-    }
+                           value={form.phone}
+                           onChange={handleChangeForm}
+                />}
+
+            <Button variant="contained" color="primary" onClick={handleSubmitForm}>
+                { form.input === 'Войти' ? 'Войти' : 'Зарегестрироватся' }
+            </Button>
+        </form>
+    )
 
 }
